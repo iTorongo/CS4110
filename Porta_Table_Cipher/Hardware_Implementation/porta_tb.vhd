1d22c1452adbb929d2833a621ecc58f706b1c969
Priyonto M Rahman <priyontoo@gmail.com>
2021-12-10 00:36:21 +0100

    Heavily optimised HW Implementation of Porta Table Cipher

--- Porta_Table_Cipher/Hardware_Implementation/porta_tb.vhd
+++ Porta_Table_Cipher/Hardware_Implementation/porta_tb.vhd
@@ -9,12 +9,11 @@ architecture arch of porta_tb is
 constant clk_period : time := 10 ns;
 constant bit_period : time := 52083ns; -- time for 1 bit.. 1bit/19200bps = 52.08 us
 
-constant rx_data_ascii_m: std_logic_vector(7 downto 0) := x"6D"; -- receive m
+constant rx_data_ascii_c: std_logic_vector(7 downto 0) := x"63"; -- receive c
+constant rx_data_ascii_h: std_logic_vector(7 downto 0) := x"68"; -- receive h
 constant rx_data_ascii_i: std_logic_vector(7 downto 0) := x"69"; -- receive i
-constant rx_data_ascii_s: std_logic_vector(7 downto 0) := x"73"; -- receive s
-constant rx_data_ascii_t: std_logic_vector(7 downto 0) := x"74"; -- receive t
 constant rx_data_ascii_e: std_logic_vector(7 downto 0) := x"65"; -- receive e
-constant rx_data_ascii_r: std_logic_vector(7 downto 0) := x"72"; -- receive r
+constant rx_data_ascii_f: std_logic_vector(7 downto 0) := x"66"; -- receive f
 constant rx_data_ascii_enter: std_logic_vector(7 downto 0) := x"0D"; -- receive enter
 
 Component porta
@@ -47,11 +46,21 @@ begin
         reset <= '0';
         wait for clk_period*2;
         
-        -- Test ASCII char m
+        -- Test ASCII char c
                 srx <= '0'; -- start bit = 0
                 wait for bit_period;
                 for i in 0 to 7 loop
-                    srx <= rx_data_ascii_m(i);   -- 8 data bits
+                    srx <= rx_data_ascii_c(i);   -- 8 data bits
+                    wait for bit_period;
+                end loop;
+                srx <= '1'; -- stop bit = 1
+                wait for 1ms;
+
+        -- Test ASCII char h
+                srx <= '0'; -- start bit = 0
+                wait for bit_period;
+                for i in 0 to 7 loop
+                    srx <= rx_data_ascii_h(i);   -- 8 data bits
                     wait for bit_period;
                 end loop;
                 srx <= '1'; -- stop bit = 1
@@ -66,45 +75,25 @@ begin
                 end loop;
                 srx <= '1'; -- stop bit = 1
                 wait for 1ms;
-
-        -- Test ASCII char s
-                srx <= '0'; -- start bit = 0
-                wait for bit_period;
-                for i in 0 to 7 loop
-                    srx <= rx_data_ascii_s(i);   -- 8 data bits
-                    wait for bit_period;
-                end loop;
-                srx <= '1'; -- stop bit = 1
-                wait for 1ms;
         
-        -- Test ASCII char t
+        -- Test ASCII char e
                 srx <= '0'; -- start bit = 0
-                wait for bit_period;
-                for i in 0 to 7 loop
-                    srx <= rx_data_ascii_t(i);   -- 8 data bits
-                    wait for bit_period;
-                end loop;
-                srx <= '1'; -- stop bit = 1
-                wait for 1ms;
- 
-         -- Test ASCII char e
-                srx <= '0';                      -- start bit = 0
                 wait for bit_period;
                 for i in 0 to 7 loop
                     srx <= rx_data_ascii_e(i);   -- 8 data bits
                     wait for bit_period;
                 end loop;
-                srx <= '1';                      -- stop bit = 1
+                srx <= '1'; -- stop bit = 1
                 wait for 1ms;
-
-        -- Test ASCII char r
-                srx <= '0'; -- start bit = 0
+ 
+         -- Test ASCII char f
+                srx <= '0';                      -- start bit = 0
                 wait for bit_period;
                 for i in 0 to 7 loop
-                    srx <= rx_data_ascii_r(i);   -- 8 data bits
+                    srx <= rx_data_ascii_f(i);   -- 8 data bits
                     wait for bit_period;
                 end loop;
-                srx <= '1'; -- stop bit = 1
+                srx <= '1';                      -- stop bit = 1
                 wait for 1ms;
                                                 
          -- Test ACII Enter
