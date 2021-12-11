294719481ab73845187bbc41b4a396ed27e1b6be
Priyonto M Rahman <priyontoo@gmail.com>
2021-12-12 00:34:51 +0100

    Improved SW Implementation for both ciphers

--- Autoclave_Cipher/Software_Implementation/AutoclaveCipherSW.c
+++ Autoclave_Cipher/Software_Implementation/AutoclaveCipherSW.c
@@ -1,4 +1,3 @@
-
 /****************************************************************************
 		Candiate No - 8019, 8012, 8007, 8026
 * @file     AutoclaveCipherSW.c
@@ -13,7 +12,6 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-
 #define MAX_TEXT_SIZE 200 // Max text size
 
 /***************************************************************************
@@ -21,164 +19,187 @@
 * @param	Encryption or Decryption Key, Input text and Output text
 * @return	Cipher/Plain text
 ****************************************************************************/
-void encryptDecrypt(const bool isEncrypt, char* key, char* inputText, char* outputText) {
+void encryptDecrypt(const bool isEncrypt, char *key, char *inputText, char *outputText)
+{
 
-	// MARK:- VARIABLES
+    // MARK:- VARIABLES
 
-	int inputTextIndex = 0; // To keep track of input plain/cipher text character index within the plain/cipher text
-    int processedText = 0; // To store cipher text
-	int messageToProcess = 0; // To calculate and store plain/cipher text character index, range of 0-25 for a-z
-	int keyIndex = 0; // To store the key index, range of 0-25 for a-z
+    int inputTextIndex = 0;   // To keep track of input plain/cipher text character index within the plain/cipher text
+    int processedText = 0;    // To store cipher text
+    int messageToProcess = 0; // To calculate and store plain/cipher text character index, range of 0-25 for a-z
+    int keyIndex = 0;         // To store the key index, range of 0-25 for a-z
 
-
-	for (inputTextIndex = 0; inputText[inputTextIndex] != '\0'; inputTextIndex++) {
-		if(inputText[inputTextIndex] == ' ') {
-			outputText[inputTextIndex] = ' '; // Handle the space
-			continue;
-		}
+    for (inputTextIndex = 0; inputText[inputTextIndex] != '\0'; inputTextIndex++)
+    {
+        if (inputText[inputTextIndex] == ' ')
+        {
+            outputText[inputTextIndex] = ' '; // Handle the space
+            continue;
+        }
 
         // Handling Uppercase
-		/// If plaintext is uppercase [A-Z], 65 is substracted to convert it into 0-25 index for simplified calculation
-		/// If plaintext is lowercase [a-z], 97 is substracted to convert it into 0-25 index for simplified calculation
-        if isupper(inputText[inputTextIndex]) {
+        /// If plaintext is uppercase [A-Z], 65 is substracted to convert it into 0-25 index for simplified calculation
+        /// If plaintext is lowercase [a-z], 97 is substracted to convert it into 0-25 index for simplified calculation
+        if isupper (inputText[inputTextIndex])
+        {
             messageToProcess = inputText[inputTextIndex] - 65;
-        } else {
+        }
+        else
+        {
             messageToProcess = inputText[inputTextIndex] - 97;
         }
 
-		// Handle key index conversion
-		/// Considering key will be always lowercased, 97 is substracted to convert it into 0-25 index for simplified calculation
-        if (key > 25) {
-         	keyIndex = key - 97;
-        } else {
-			keyIndex = key;
-		}
+        // Handle key index conversion
+        /// Considering key will be always lowercased, 97 is substracted to convert it into 0-25 index for simplified calculation
+        if (key > 25)
+        {
+            keyIndex = key - 97;
+        }
+        else
+        {
+            keyIndex = key;
+        }
 
-		// The encryption equation:- (Pi + Ki)
-		/// Pi = Index of plaintext character at i position, Ki = Index of key character at i position
-		// In Autoclave cipher encryption - The ciphertext becomes the key for the next plaintext encryption
-		/// Hence once encryption of a plaintext is complete, the key is updated with the encrypted message
+        // The encryption equation:- (Pi + Ki) mod 26
+        /// Pi = Index of plaintext character at i position, Ki = Index of key character at i position
+        // In Autoclave cipher encryption - The ciphertext becomes the key for the next plaintext encryption
+        /// Hence once encryption of a plaintext is complete, the key is updated with the encrypted message
 
-		// The decryption equation:- (Ci - Ki)
-		/// Ci = Index of ciphertext character at i position, Ki = Index of key character at i position
-		/// In Autoclave cipher decryption - The ciphertext becomes the key for the next ciphertext decryption
-		/// e.g. if key b is used to decrypt message swpjan, first 'b' will be used for 's' to decrypt, then 's' will be used as key to decrypt next plaintext 'w' and so on
-		/// Hence once decryption of a ciphertext is complete, the key is updated with the ciphertext - the one just decrypted
-        
-		if (isEncrypt) {
-			processedText = messageToProcess + keyIndex;
-			if (processedText > 25) {
-            	processedText = processedText - 26;
-        	}
-        	key = processedText; // Update the key
-		} else {
-			processedText = messageToProcess - keyIndex;
-			if (processedText < 0) {
-            	processedText = processedText + 26;
-        	}
-        	key = inputText[inputTextIndex]; // Update the key
-		}
+        // The decryption equation:- (Ci - Ki) mod 26
+        /// Ci = Index of ciphertext character at i position, Ki = Index of key character at i position
+        /// In Autoclave cipher decryption - The ciphertext becomes the key for the next ciphertext decryption
+        /// e.g. if key b is used to decrypt message swpjan, first 'b' will be used for 's' to decrypt, then 's' will be used as key to decrypt next plaintext 'w' and so on
+        /// Hence once decryption of a ciphertext is complete, the key is updated with the ciphertext - the one just decrypted
 
-		// Handling uppercase/lowercase input
-        if isupper(inputText[inputTextIndex]) {
+        if (isEncrypt)
+        {
+            processedText = (messageToProcess + keyIndex) % 26;
+            key = processedText; // Update the key
+        }
+        else
+        {
+            processedText = (messageToProcess - keyIndex + 26) % 26;
+            key = inputText[inputTextIndex]; // Update the key
+        }
+
+        // Handling uppercase/lowercase input
+        if isupper (inputText[inputTextIndex])
+        {
             processedText = processedText + 'A'; // If uppercase
-        } else {
+        }
+        else
+        {
             processedText = processedText + 'a'; // If lowercase
         }
-		
-		outputText[inputTextIndex] = (char)processedText; // Assiging processedText character
-	}
-	outputText[inputTextIndex] = '\0';
-	return;
+
+        outputText[inputTextIndex] = (char)processedText; // Assiging processedText character
+    }
+    outputText[inputTextIndex] = '\0';
+    return;
 }
-
-
 
 /***************************************************************************
 * Main function to take user inputs and encrypt/decrypt them based on selection
 * @param	None
 * @return	None
 ****************************************************************************/
-int main() {
+int main()
+{
 
-	// MARK:- VARIABLES
-	char inputText[MAX_TEXT_SIZE] = {'\0'}; // To store inserted text from user
-	char convertedText[MAX_TEXT_SIZE] = {'\0'}; // To store converted text
-	char selection = '\0'; // To store users selection
-	char key = 'b'; // Hardcoded key 'b' for encryption 
-	
-	while (1) {
-		char temp; 
-		while (selection != 'e' && selection != 'd' && selection != 'q') {
-			printf("\n****Autoclave Cipher****\nPress 'e' for encrypt or 'd' for decrypt, 'q' for quit: "); 
-			scanf("%c%c", &selection, &temp);
-		}
-		if (selection == 'q') {
-			printf("\n Program finished.");
-			return 0;
-		}
-		// Handle the selection
-		/// e = encryption; d = decryption
-		if (selection == 'e' || selection == 'd' ) {
-			if (selection == 'e') {
-				printf("\r\nEnter the plain text:\r\n");
-			}
-			else {
-				printf("\r\nEnter the cipher text:\r\n");
-			}
+    // MARK:- VARIABLES
+    char inputText[MAX_TEXT_SIZE] = {'\0'};     // To store inserted text from user
+    char convertedText[MAX_TEXT_SIZE] = {'\0'}; // To store converted text
+    char selection = '\0';                      // To store users selection
+    char key = 'b';                             // Hardcoded key 'b' for encryption
 
-			int i = 0;
-			while((temp = getchar())) {
-                if ( (temp >= 'A' && temp <= 'Z') || (temp >= 'a' && temp <= 'z') || temp == ' ' ) {
+    while (1)
+    {
+        char temp;
+        while (selection != 'e' && selection != 'd' && selection != 'q')
+        {
+            printf("\n****Autoclave Cipher****\nPress 'e' for encrypt or 'd' for decrypt, 'q' for quit: ");
+            scanf("%c%c", &selection, &temp);
+        }
+        if (selection == 'q')
+        {
+            printf("\n Program finished.");
+            return 0;
+        }
+        // Handle the selection
+        /// e = encryption; d = decryption
+        if (selection == 'e' || selection == 'd')
+        {
+            if (selection == 'e')
+            {
+                printf("\r\nEnter the plain text:\r\n");
+            }
+            else
+            {
+                printf("\r\nEnter the cipher text:\r\n");
+            }
+
+            int i = 0;
+            while ((temp = getchar()))
+            {
+                if ((temp >= 'A' && temp <= 'Z') || (temp >= 'a' && temp <= 'z') || temp == ' ')
+                {
                     inputText[i] = temp;
-                } else {
-					// Handle new line and carriage return
-					/// If none of those, it's invalid input as we will encrypt/decrypt a-z and A-Z
-                    if (temp == '\n' || temp == '\r') {
-                        inputText[i]='\0';
+                }
+                else
+                {
+                    // Handle new line and carriage return
+                    /// If none of those, it's invalid input as we will encrypt/decrypt a-z and A-Z
+                    if (temp == '\n' || temp == '\r')
+                    {
+                        inputText[i] = '\0';
                         break;
-                    } else {
+                    }
+                    else
+                    {
                         printf("Please enter valid characters e.g [a-z or A-Z]\r\n");
-				        continue;
+                        continue;
                     }
                 }
 
-				inputText[i] = temp;
-				i++;
-				if ( i == MAX_TEXT_SIZE -2 ) {
-					printf("max size reached\r\n");
-					i++;
-					inputText[i]='\0';
-					break;
-				}
-			}
-			
-			// Encryption/Decryption
-			if (selection == 'e') {
-				printf("\r\nThe plain text:\r\n");
-			}
-			else {
-				printf("\r\nThe cipher text:\r\n");
-				
-			}
-			printf("%s", inputText);
-			
-			// Encryption/Decryption
-			if (selection == 'e') {
-			    encryptDecrypt(true, key, inputText, convertedText); // Encryption method - Key, inputText and convertedText variables are passed as arguments.
-				printf("\r\nThe cipher text:\r\n");
-				printf("%s", convertedText); // prints cipher text. 
-			}
-			else {
-			    encryptDecrypt(false, key, inputText, convertedText); // Decryption method - inputText and convertedText variables are passed as arguments.
-				printf("\r\nThe plain text:\r\n");
-				printf("%s", convertedText); // prints plain text
-			    
-			}
-			printf("\r\n");
-		}
-		selection = '\0';
-	}
-	return 0;
-	cleanup_platform();
+                inputText[i] = temp;
+                i++;
+                if (i == MAX_TEXT_SIZE - 2)
+                {
+                    printf("max size reached\r\n");
+                    i++;
+                    inputText[i] = '\0';
+                    break;
+                }
+            }
+
+            // Encryption/Decryption
+            if (selection == 'e')
+            {
+                printf("\r\nThe plain text:\r\n");
+            }
+            else
+            {
+                printf("\r\nThe cipher text:\r\n");
+            }
+            printf("%s", inputText);
+
+            // Encryption/Decryption
+            if (selection == 'e')
+            {
+                encryptDecrypt(true, key, inputText, convertedText); // Encryption method - Key, inputText and convertedText variables are passed as arguments.
+                printf("\r\nThe cipher text:\r\n");
+                printf("%s", convertedText); // prints cipher text.
+            }
+            else
+            {
+                encryptDecrypt(false, key, inputText, convertedText); // Decryption method - inputText and convertedText variables are passed as arguments.
+                printf("\r\nThe plain text:\r\n");
+                printf("%s", convertedText); // prints plain text
+            }
+            printf("\r\n");
+        }
+        selection = '\0';
+    }
+    return 0;
+    cleanup_platform();
 }
--- Porta_Table_Cipher/Software_Implementation/PortaTableCipherSW.c
+++ Porta_Table_Cipher/Software_Implementation/PortaTableCipherSW.c
@@ -11,164 +11,186 @@
 #include <stdio.h>
 #include <ctype.h>
 
-#define MAX_TEXT_SIZE 200 // Max text size
+#define MAX_TEXT_SIZE 200                     // Max text size
 #define CIPHER_KEY "mistermistermistermister" // Hardcoded key for encryption/decryption process
-#define CIPHER_KEY_LEN 24 // Hardcoded key length
-
-
+#define CIPHER_KEY_LEN 24                     // Hardcoded key length
 
 /***************************************************************************
 * Return the encrypted/decrypted text generated with the help of the key
 * @param	Input text and Output text
 * @return	Cipher/Plain text
 ****************************************************************************/
-void encryptDecrypt(const char* inputText, char* outputText) {
-	// MARK:- VARIABLES
-	char key[CIPHER_KEY_LEN] = CIPHER_KEY; // Assign hardcoded key to the variable
-	int inputTextCounter, keyCounter = 0; // Necessary counter variables
-    int convertedText = 0; // To store processed text
-	int messageToProcess = 0; // To calculate and store input text character index, range of 0-25 for a-z
+void encryptDecrypt(const char *inputText, char *outputText)
+{
+    // MARK:- VARIABLES
+    char key[CIPHER_KEY_LEN] = CIPHER_KEY; // Assign hardcoded key to the variable
+    int inputTextCounter, keyCounter = 0;  // Necessary counter variables
+    int convertedText = 0;                 // To store processed text
+    int messageToProcess = 0;              // To calculate and store input text character index, range of 0-25 for a-z
 
-	for (inputTextCounter = 0; inputText[inputTextCounter] != '\0'; inputTextCounter++) {
-		if(inputText[inputTextCounter] == ' ') {
-			outputText[inputTextCounter] = ' '; // Handle spacing
-			continue;
-		}
+    for (inputTextCounter = 0; inputText[inputTextCounter] != '\0'; inputTextCounter++)
+    {
+        if (inputText[inputTextCounter] == ' ')
+        {
+            outputText[inputTextCounter] = ' '; // Handle spacing
+            continue;
+        }
 
-		// Handling Uppercase
-		/// If inputtext is uppercase [A-Z], 65 is substracted to convert it into 0-25 index for simplified calculation
-		/// If inputtext is lowercase [a-z], 97 is substracted to convert it into 0-25 index for simplified calculation
-        if isupper(inputText[inputTextCounter]) {
+        // Handling Uppercase
+        /// If inputtext is uppercase [A-Z], 65 is substracted to convert it into 0-25 index for simplified calculation
+        /// If inputtext is lowercase [a-z], 97 is substracted to convert it into 0-25 index for simplified calculation
+        if isupper (inputText[inputTextCounter])
+        {
             messageToProcess = inputText[inputTextCounter] - 65;
-        } else {
+        }
+        else
+        {
             messageToProcess = inputText[inputTextCounter] - 97;
         }
 
-		// Handle key index conversion
-		/// Considering key will be always lowercased as its hardcoded, 97 is substracted to convert it into 0-25 index for simplified calculation
+        // Handle key index conversion
+        /// Considering key will be always lowercased as its hardcoded, 97 is substracted to convert it into 0-25 index for simplified calculation
         int keyIndex = key[keyCounter] - 97;
 
-		// Encryption/Decryption Process:-
-		/// We figured out a formula for the encryption and decryption of the porta table cipher
-		/// The same formula works for both encryption and decryption as they are basically same for this cipher
-		/// The formula:- 
-		/// If message < 13 { return 13 + (message - (k/2)) mod 13}
-		/// else {return (message + (k/2)) mod 13}
-		/// message and key both are considered index of respective character. e.g. a-0, b=1, z=25
+        // Encryption/Decryption Process:-
+        /// We figured out a formula for the encryption and decryption of the porta table cipher
+        /// The same formula works for both encryption and decryption as they are basically same for this cipher
+        /// The formula:-
+        /// If message < 13 { return 13 + (message - (k/2)) mod 13}
+        /// else {return (message + (k/2)) mod 13}
+        /// message and key both are considered index of respective character. e.g. a-0, b=1, z=25
 
-        if (messageToProcess < 13) { // if message index < 13
+        if (messageToProcess < 13)
+        { // if message index < 13
             int mod = 0;
-            int difference = messageToProcess - (keyIndex/2);
-            if (difference > 0) {
-                mod = difference % 13;
-                convertedText = 13 + mod;
-            } else {
-                mod = difference + 13;
-                convertedText = 13 + mod;
-            }
-        } else {
-            convertedText = (messageToProcess + (keyIndex/2)) % 13; // if message index >= 13
+            int difference = messageToProcess - (keyIndex / 2);
+            convertedText = 13 + (difference + 13) % 13;
+        }
+        else
+        {
+            convertedText = (messageToProcess + (keyIndex / 2)) % 13; // if message index >= 13
         }
 
-		// Handling uppercase/lowercase input
-        if isupper(inputText[inputTextCounter]) {
+        // Handling uppercase/lowercase input
+        if isupper (inputText[inputTextCounter])
+        {
             convertedText = convertedText + 'A'; // If uppercase
-        } else {
+        }
+        else
+        {
             convertedText = convertedText + 'a'; // If lowercase
         }
 
-		outputText[inputTextCounter] = (char)convertedText; // Assiging converted text input output text based on index
-		keyCounter++;
-		keyCounter = (keyCounter > CIPHER_KEY_LEN-1) ? 0 : keyCounter; // If keyCounter greated than predefined key length make the keyCounter to 0, else keyCounter will remain same
-	}
-	outputText[inputTextCounter] = '\0';
-	return;
+        outputText[inputTextCounter] = (char)convertedText; // Assiging converted text input output text based on index
+        keyCounter++;
+        keyCounter = (keyCounter > CIPHER_KEY_LEN - 1) ? 0 : keyCounter; // If keyCounter greated than predefined key length make the keyCounter to 0, else keyCounter will remain same
+    }
+    outputText[inputTextCounter] = '\0';
+    return;
 }
-
 
 /***************************************************************************
 * Main function to take user inputs and encrypt/decrypt them based on selection
 * @param	None
 * @return	None
 ****************************************************************************/
-int main() {
+int main()
+{
 
-	// MARK:- VARIABLES
-	char inputText[MAX_TEXT_SIZE] = {'\0'}; // To store inserted text from user
-	char convertedText[MAX_TEXT_SIZE] = {'\0'}; // To store converted text
-	char selection = '\0'; // To store users selection
-	while (1) {
-		char temp;
-		while (selection != 'e' && selection != 'd' && selection != 'q') {
-			printf("\n****Porta Table Cipher****\nPress 'e' for encrypt or 'd' for decrypt, 'q' for quit: "); // this printfs twice after first selection
-			scanf("%c%c", &selection, &temp);
-		}
-		if (selection == 'q') {
-			printf("\n Program finished.");
-			return 0;
-		}
+    // MARK:- VARIABLES
+    char inputText[MAX_TEXT_SIZE] = {'\0'};     // To store inserted text from user
+    char convertedText[MAX_TEXT_SIZE] = {'\0'}; // To store converted text
+    char selection = '\0';                      // To store users selection
+    while (1)
+    {
+        char temp;
+        while (selection != 'e' && selection != 'd' && selection != 'q')
+        {
+            printf("\n****Porta Table Cipher****\nPress 'e' for encrypt or 'd' for decrypt, 'q' for quit: "); // this printfs twice after first selection
+            scanf("%c%c", &selection, &temp);
+        }
+        if (selection == 'q')
+        {
+            printf("\n Program finished.");
+            return 0;
+        }
 
-		// Handle the selection
-		/// e = encryption; d = decryption
-		if (selection == 'e' || selection == 'd' ) { 
-			if (selection == 'e') {
-				printf("\r\nEnter the plain text:\r\n");
-			}
-			else {
-				printf("\r\nEnter the cipher text:\r\n");
-			}
-			int i = 0;
-			while((temp = getchar())) {
+        // Handle the selection
+        /// e = encryption; d = decryption
+        if (selection == 'e' || selection == 'd')
+        {
+            if (selection == 'e')
+            {
+                printf("\r\nEnter the plain text:\r\n");
+            }
+            else
+            {
+                printf("\r\nEnter the cipher text:\r\n");
+            }
+            int i = 0;
+            while ((temp = getchar()))
+            {
 
-                if ( (temp >= 'A' && temp <= 'Z') || (temp >= 'a' && temp <= 'z') || temp == ' ' ) {
+                if ((temp >= 'A' && temp <= 'Z') || (temp >= 'a' && temp <= 'z') || temp == ' ')
+                {
                     inputText[i] = temp;
-                } else {
-					// Handle new line and carriage return
-					/// If none of those, it's invalid input as we will encrypt/decrypt a-z and A-Z
-                    if (temp == '\n' || temp == '\r') {
-                        inputText[i]='\0';
+                }
+                else
+                {
+                    // Handle new line and carriage return
+                    /// If none of those, it's invalid input as we will encrypt/decrypt a-z and A-Z
+                    if (temp == '\n' || temp == '\r')
+                    {
+                        inputText[i] = '\0';
                         break;
-                    } else {
+                    }
+                    else
+                    {
                         printf("Please enter valid characters e.g [a-z or A-Z]\r\n");
-				        continue;
+                        continue;
                     }
                 }
 
-				inputText[i] = temp;
-				i++;
-				if ( i == MAX_TEXT_SIZE -2 ) {
-					printf("max size reached\r\n");
-					i++;
-					inputText[i]='\0';
-					break;
-				}
-			}
-			
-			if (selection == 'e') {
-				printf("\r\nThe plain text:\r\n");
-			}
-			else {
-				printf("\r\nThe cipher text:\r\n");
-			}
-			printf("%s", inputText);
+                inputText[i] = temp;
+                i++;
+                if (i == MAX_TEXT_SIZE - 2)
+                {
+                    printf("max size reached\r\n");
+                    i++;
+                    inputText[i] = '\0';
+                    break;
+                }
+            }
 
-			// Encryption/Decryption
-			/// A single method is called with inputText and convertedText variable as arguments
-			/// because the encryption and decryption method is same for this cipher
-			/// this method computes the encrypted or decrypted message and returns
+            if (selection == 'e')
+            {
+                printf("\r\nThe plain text:\r\n");
+            }
+            else
+            {
+                printf("\r\nThe cipher text:\r\n");
+            }
+            printf("%s", inputText);
 
-			encryptDecrypt(inputText, convertedText);
-			if (selection == 'e') {
-				printf("\r\nThe cipher text:\r\n");
-			}
-			else {
-				printf("\r\nThe plain text:\r\n");
-			}
-			printf("%s", convertedText); // Prints the encrypted/decrypted message
-			printf("\r\n");
-		}
-		selection = '\0';
-	}
-	return 0;
-	cleanup_platform();
+            // Encryption/Decryption
+            /// A single method is called with inputText and convertedText variable as arguments
+            /// because the encryption and decryption method is same for this cipher
+            /// this method computes the encrypted or decrypted message and returns
+
+            encryptDecrypt(inputText, convertedText);
+            if (selection == 'e')
+            {
+                printf("\r\nThe cipher text:\r\n");
+            }
+            else
+            {
+                printf("\r\nThe plain text:\r\n");
+            }
+            printf("%s", convertedText); // Prints the encrypted/decrypted message
+            printf("\r\n");
+        }
+        selection = '\0';
+    }
+    return 0;
+    cleanup_platform();
 }
