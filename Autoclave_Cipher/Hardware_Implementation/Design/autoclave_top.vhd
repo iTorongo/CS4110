bfa5b8f2bffaef53a8a0f56748b82b5354c4803a
Priyonto M Rahman <priyontoo@gmail.com>
2021-12-13 00:21:20 +0100

    Set initial key to register

--- Autoclave_Cipher/Hardware_Implementation/Design/autoclave_top.vhd
+++ Autoclave_Cipher/Hardware_Implementation/Design/autoclave_top.vhd
@@ -1,120 +1,132 @@
--- (adapted from) Listing 7.4
+----------------------------------------------------------------------------------
+-- Top design module
+----------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 entity autoclave is
-   generic(
-     -- Default setting:
-     -- 19,200 baud, 8 data bis, 1 stop bits
-      DBIT: integer:=8;     -- # data bits
-      SB_TICK: integer:=16; -- # ticks for stop bits, 16/24/32: for 1/1.5/2 stop bits
-      DVSR: integer:= 326;  -- baud rate divisor: DVSR = 100M/(16*baud rate)
-      DVSR_BIT: integer:=9; -- # bits of DVSR
-      ROM_ADDR_WIDTH: integer:=5;   -- # maximum size of the ROM (key): 2^5 (32)
-      KEY_LENGTH: integer:=19;       -- length of the key currently used
-	  RAM_ADDR_WIDTH: integer:=10;  -- # maximum size of the RAM: 2^10 (1024)
-	  RAM_DATA_WIDTH: integer:=8   -- # 8-bit data words
-   );
-   port(
-      clk, reset, dec: in std_logic;
-      rx: in std_logic;
-      tx: out std_logic
-   );
+  generic(
+    -- Default setting:
+    -- 19,200 baud, 8 data bis, 1 stop bits
+     DBIT: integer:=8;     -- # data bits
+     SB_TICK: integer:=16; -- # ticks for stop bits, 16/24/32: for 1/1.5/2 stop bits
+     DVSR: integer:= 326;  -- baud rate divisor: DVSR = 100M/(16*baud rate)
+     DVSR_BIT: integer:=9; -- # bits of DVSR
+     ROM_ADDR_WIDTH: integer:=5;   -- # maximum size of the ROM (key): 2^5 (32)
+     KEY_LENGTH: integer:=19;       -- length of the key currently used
+    RAM_ADDR_WIDTH: integer:=10;  -- # maximum size of the RAM: 2^10 (1024)
+    RAM_DATA_WIDTH: integer:=8   -- # 8-bit data words
+  );
+  port(
+     clk, reset, dec: in std_logic;
+     rx: in std_logic;
+     tx: out std_logic
+  );
 end autoclave;
-
-architecture str_arch of autoclave is
-   signal tick: std_logic;
-   signal rx_done: std_logic;
-   signal ascii_r, ascii_t: std_logic_vector(7 downto 0);
-   signal tx_start, tx_done: std_logic;
-   signal clrb_ram, incb_ram: std_logic;
-   signal addrb_ram: std_logic_vector(9 downto 0);
-   signal key, cphr_out: std_logic_vector(7 downto 0);
-   signal wr: std_logic;
-   signal load_reg_a, clear_reg_a: std_logic;
-   signal reg_a_out: std_logic_vector(7 downto 0);
-   signal load_reg_b, clear_reg_b: std_logic;
-   signal reg_b_out: std_logic_vector(7 downto 0);
-   signal mux_ctr_key: std_logic;
-   signal mux_ctr_input: std_logic;
-   signal dec_mode: std_logic;
-begin
-   
-   key <= reg_a_out when ((mux_ctr_key = '1') and (dec_mode = '0')) else 
-      x"62" when ((mux_ctr_key = '0') and (dec_mode = '0')) else -- encode
-
-      reg_b_out when ((mux_ctr_input = '1') and (dec_mode = '1')) else
-      x"62" when ((mux_ctr_input = '0') and (dec_mode = '1')); -- decode
-
-   baud_gen_unit: entity work.mod_m_counter(arch)
-      generic map(M=>DVSR, N=>DVSR_BIT)
-      port map(
-	  clk=>clk, reset=>reset,
-      q=>open, max_tick=>tick
-	  );
-
-   uart_rx_unit: entity work.uart_rx(arch)
-      generic map(DBIT=>DBIT, SB_TICK=>SB_TICK)
-      port map(
-	  clk=>clk, reset=>reset, rx=>rx,
-      s_tick=>tick, rx_done_tick=>rx_done,
-      dout=>ascii_r
-	  );
-
-   uart_tx_unit: entity work.uart_tx(arch)
-      generic map(DBIT=>DBIT, SB_TICK=>SB_TICK)
-      port map(
-	  clk=>clk, reset=>reset,
-      tx_start=>tx_start,
-      s_tick=>tick, din=>ascii_t,
-      tx_done_tick=> tx_done, tx=>tx
-	  );
-   
-   cnt_ram_unit: entity work.cnt_ram(arch)
-      generic map(N=>RAM_ADDR_WIDTH)
-      port map(
-      clk=>clk, reset=>reset,
-      syn_clr=>clrB_ram, load=>'0', en=>incb_ram, up=>'1',
-      d=>(others=>'0'),
-      max_tick=>open, min_tick=>open,
-      q=>addrB_ram   
-	  );
-
-   ram_unit: entity work.xilinx_one_port_ram_sync(arch)
-      generic map(ADDR_WIDTH=>RAM_ADDR_WIDTH, DATA_WIDTH=>RAM_DATA_WIDTH)
-      port map(
-      clk=>clk, wr=>wr,
-      addr=>addrb_ram,
-      din=>cphr_out, dout=>ascii_t	  
-	  );
-
-   reg_a : ENTITY work.reg8bits(arch)
-     PORT MAP(
-        clk => clk, rst => reset, load => load_reg_a,
-        clear => clear_reg_a, d => cphr_out, q => reg_a_out);
-
-   reg_b : ENTITY work.reg8bits(arch)
-     PORT MAP(
-        clk => clk, rst => reset, load => load_reg_b,
-        clear => clear_reg_b, d => ascii_r, q => reg_b_out);
-
-   cipher_unit: entity work.cipher(arch)
-      port map(
-      ascii_r=>ascii_r, key=>key, dec => dec_mode,
-      cphr_out=>cphr_out	  
-	  );			   
  
-   ctr_path_unit: entity work.ctr_path(arch)
-      port map(
-      clk=>clk, reset=>reset, dec_mode => dec_mode, dec=> dec,
-      mux_ctr_key => mux_ctr_key,
-      mux_ctr_input => mux_ctr_input,
-      rx_done=>rx_done, ascii_r=>ascii_r, 
-      clrb_ram=>clrb_ram, incb_ram=>incb_ram, 
-      wr=>wr, ascii_t=>ascii_t, 
-      tx_start=>tx_start, tx_done=>tx_done,
-      load_reg_a => load_reg_a, clear_reg_a => clear_reg_a,
-      load_reg_b => load_reg_b, clear_reg_b => clear_reg_b
-	  );	
-			   
+architecture str_arch of autoclave is
+  -- Baud and uart signals
+  signal tick: std_logic;
+  signal rx_done: std_logic;
+  signal tx_start, tx_done: std_logic;
+  signal ascii_r, ascii_t: std_logic_vector(7 downto 0);
+ 
+  -- RAM signals
+  signal clra_ram, inca_ram: std_logic;
+  signal addra_ram: std_logic_vector(9 downto 0);
+  signal wr: std_logic;
+ 
+  -- Encoder and decoder signals
+  signal key, cphr_out: std_logic_vector(7 downto 0);
+  signal load_reg_a, clear_reg_a: std_logic;
+  signal load_reg_b, clear_reg_b: std_logic;
+  signal load_reg_k, clear_reg_k: std_logic;
+  signal reg_a_out, reg_b_out, reg_k_out: std_logic_vector(7 downto 0);
+  signal mux_ctr_a, mux_ctr_b: std_logic;
+  signal dec_mode: std_logic;
+begin
+  -- Assigning the value of key based on encode and decode condition dec_mode = 0 represents encode
+  key <= reg_a_out when ((mux_ctr_a = '1') and (dec_mode = '0')) else  -- key will be reg_a_out when encode enabled
+     reg_k_out when ((mux_ctr_a = '0') and (dec_mode = '0')) else  -- initial key value from register 'K'
+     reg_b_out when ((mux_ctr_b = '1') and (dec_mode = '1')) else -- key will be reg_b_out when devode enabled
+     reg_k_out when ((mux_ctr_b = '0') and (dec_mode = '1')); -- initial key value from register 'K'
+ 
+  baud_gen_unit: entity work.mod_m_counter(arch)
+     generic map(M=>DVSR, N=>DVSR_BIT)
+     port map(
+    clk=>clk, reset=>reset,
+     q=>open, max_tick=>tick
+    );
+ 
+  uart_rx_unit: entity work.uart_rx(arch)
+     generic map(DBIT=>DBIT, SB_TICK=>SB_TICK)
+     port map(
+    clk=>clk, reset=>reset, rx=>rx,
+     s_tick=>tick, rx_done_tick=>rx_done,
+     dout=>ascii_r
+    );
+ 
+  uart_tx_unit: entity work.uart_tx(arch)
+     generic map(DBIT=>DBIT, SB_TICK=>SB_TICK)
+     port map(
+    clk=>clk, reset=>reset,
+     tx_start=>tx_start,
+     s_tick=>tick, din=>ascii_t,
+     tx_done_tick=> tx_done, tx=>tx
+    );
+ 
+  cnt_ram_unit: entity work.cnt_ram(arch)
+     generic map(N=>RAM_ADDR_WIDTH)
+     port map(
+     clk=>clk, reset=>reset,
+     syn_clr=>clra_ram, load=>'0', en=>inca_ram, up=>'1',
+     d=>(others=>'0'),
+     max_tick=>open, min_tick=>open,
+     q=>addra_ram  
+    );
+ 
+  ram_unit: entity work.xilinx_one_port_ram_sync(arch)
+     generic map(ADDR_WIDTH=>RAM_ADDR_WIDTH, DATA_WIDTH=>RAM_DATA_WIDTH)
+     port map(
+     clk=>clk, wr=>wr,
+     addr=>addra_ram,
+     din=>cphr_out, dout=>ascii_t   
+    );
+ 
+  reg_a : ENTITY work.reg8bits(arch)
+    PORT MAP(
+       clk => clk, rst => reset, load => load_reg_a,
+       clear => clear_reg_a, d => cphr_out, q => reg_a_out);
+ 
+  reg_b : ENTITY work.reg8bits(arch)
+    PORT MAP(
+       clk => clk, rst => reset, load => load_reg_b,
+       clear => clear_reg_b, d => ascii_r, q => reg_b_out);
+ 
+  reg_k : ENTITY work.reg8bits(arch)
+     PORT MAP(
+     clk => clk, rst => reset, load => load_reg_k,
+     clear => clear_reg_k, d => x"62" , q => reg_k_out); -- x"62" = initial value for key 'b'
+ 
+ 
+  cipher_unit: entity work.cipher(arch)
+     port map(
+     ascii_r=>ascii_r, key=>key, dec => dec_mode,
+     cphr_out=>cphr_out    
+    );          
+  ctr_path_unit: entity work.ctr_path(arch)
+     port map(
+     clk=>clk, reset=>reset, dec_mode => dec_mode, dec=> dec,
+     mux_ctr_a => mux_ctr_a,
+     mux_ctr_b => mux_ctr_b,
+     rx_done=>rx_done, ascii_r=>ascii_r,
+     clra_ram=>clra_ram, inca_ram=>inca_ram,
+     wr=>wr, ascii_t=>ascii_t,
+     tx_start=>tx_start, tx_done=>tx_done,
+     load_reg_a => load_reg_a, clear_reg_a => clear_reg_a,
+     load_reg_b => load_reg_b, clear_reg_b => clear_reg_b,
+     load_reg_k => load_reg_k, clear_reg_k => clear_reg_k
+    ); 
+          
 end str_arch;
+
--- Autoclave_Cipher/Hardware_Implementation/Design/fsm.vhd
+++ Autoclave_Cipher/Hardware_Implementation/Design/fsm.vhd
@@ -1,117 +1,144 @@
--- (adapted from) Listing 5.1
+----------------------------------------------------------------------------------
+-- Control Path
+----------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 entity ctr_path is
-   port(
-      clk, reset: in std_logic;
-      dec : in std_logic;
-      rx_done, tx_done: in std_logic;
-	   ascii_r, ascii_t: in std_logic_vector(7 downto 0);
-      clrb_ram, incb_ram, wr, tx_start: out std_logic;
-      load_reg_a, clear_reg_a : out std_logic;
-      mux_ctr_key: out std_logic;
-      load_reg_b, clear_reg_b : out std_logic;
-      mux_ctr_input: out std_logic;
-      dec_mode : out std_logic
-   );
+  port(
+     clk, reset: in std_logic;
+     dec : in std_logic;
+     rx_done, tx_done: in std_logic;
+     ascii_r, ascii_t: in std_logic_vector(7 downto 0);
+     clra_ram, inca_ram, wr, tx_start: out std_logic;
+     load_reg_a, clear_reg_a : out std_logic;
+     load_reg_b, clear_reg_b : out std_logic;
+     load_reg_k, clear_reg_k : out std_logic;
+     mux_ctr_a, mux_ctr_b: out std_logic;
+     dec_mode : out std_logic
+  );
 end ctr_path;
-
+ 
 architecture arch of ctr_path is
-   type state_type is (s0, s1, s2, s3, s4);
-   signal state_reg, state_next: state_type;
+  type state_type is (s0, s1, s2, s3, s4);
+  signal state_reg, state_next: state_type;
 begin
-
-   -- state register
-   process(clk,reset)
-   begin
-      if (reset='1') then
-         state_reg <= s0;
-      elsif rising_edge(clk) then
-         state_reg <= state_next;
-      end if;
-   end process;
-   
-   -- next-state and outputs logic
-   process(state_reg, dec, rx_done, tx_done, ascii_r, ascii_t)
-   begin
-   clrb_ram <= '0';
-   incb_ram <= '0';
-   mux_ctr_key <= '0';
-   mux_ctr_input <= '0';
-   wr <= '0';
-   tx_start <= '0';
-   load_reg_a <= '0';
-   clear_reg_a <= '0';
-   load_reg_b <= '0';
-   clear_reg_b <= '0';
-   state_next <= state_reg;
-   
-      case state_reg is
-         when s0 =>
-			   clrb_ram <= '1';
-            state_next <= s1;
-         when s1 =>
-            if (rx_done='1') then
-               if ((ascii_r >= x"41" and ascii_r <= x"5A") or 
-                  (ascii_r >= x"61" and ascii_r <= x"7A") or 
-                     ascii_r = x"20" or ascii_r = x"0D") then
-                     wr <= '1';
-                     
-                     if (ascii_r = x"20") then
-                        incb_ram <= '1';
-                        state_next <= s1;
-                     else
-                        if (ascii_r=x"0D") then
-                           clrb_ram <= '1';
-                           state_next <= s3;
-                        else
-                           incb_ram <= '1';
-                           load_reg_a <= '1'; -- for encryption
-                           load_reg_b <= '1'; -- for decryption
-                           state_next <= s2;
-                        end if;
-                     end if;
-               end if;
-			end if;
-         when s2 =>
-            if (rx_done='1') then
-               if ((ascii_r >= x"41" and ascii_r <= x"5A") or 
-                  (ascii_r >= x"61" and ascii_r <= x"7A") or 
-                     ascii_r = x"20" or ascii_r = x"0D") then
-                     wr <= '1';
-                     
-                     if (ascii_r = x"20") then
-                        incb_ram <= '1';
-                        state_next <= s2;
-                     else
-                        if (ascii_r=x"0D") then
-                           clrb_ram <= '1';
-                           state_next <= s3;
-                        else
-                           incb_ram <= '1';
-                           mux_ctr_key <= '1'; -- encryption
-                           mux_ctr_input <= '1'; -- decryption
-                           load_reg_a <= '1';
-                           load_reg_b <= '1';
-                        end if;
-                     end if;
-               end if;
-			end if;
-         when s3 =>
-            if (ascii_t=x"0D") then 
-			   state_next <= s0;
-			else 
-			   tx_start <= '1';
-			   state_next <= s4;
-			end if;
-		 when s4 =>
-		    if (tx_done='1') then  
-			   incb_ram <= '1';
-			   state_next <= s3;
-			end if;
-      end case;
-   end process;
-
-   dec_mode <= dec;
-
+ 
+  -- state register
+  process(clk,reset)
+  begin
+     if (reset='1') then
+        state_reg <= s0;
+     elsif rising_edge(clk) then
+        state_reg <= state_next;
+     end if;
+  end process;
+ 
+  -- next-state and outputs logic
+  process(state_reg, dec, rx_done, tx_done, ascii_r, ascii_t)
+  begin
+  -- Reset
+  clra_ram <= '0';
+  inca_ram <= '0';
+  mux_ctr_a <= '0';
+  mux_ctr_b <= '0';
+  wr <= '0';
+  tx_start <= '0';
+  load_reg_a <= '0';
+  clear_reg_a <= '0';
+  load_reg_b <= '0';
+  clear_reg_b <= '0';  
+  load_reg_k <= '0';
+  clear_reg_k <= '0';
+  state_next <= state_reg;
+ 
+     case state_reg is
+        -----------------
+        -- S0
+        -----------------
+        when s0 =>
+           clra_ram <= '1'; -- clear ram key
+           load_reg_k <= '1'; -- load register
+           state_next <= s1;
+        ------------------- 
+        -- S1
+        -- Run once in flow
+        -----------------
+        when s1 =>
+           if (rx_done='1') then
+              -- Check for valid ascii only for Alphabet lower and upper case, Space and Enter
+              if ((ascii_r >= x"41" and ascii_r <= x"5A") or
+                 (ascii_r >= x"61" and ascii_r <= x"7A") or
+                    ascii_r = x"20" or ascii_r = x"0D") then
+                    wr <= '1'; -- Write enabled RAM
+ 
+                    if (ascii_r = x"20") then -- X"20" = "Space"-key
+                       inca_ram <= '1';
+                       state_next <= s1;
+                    else
+                       if (ascii_r=x"0D") then -- X"0D" = "Enter"-key (carriage return)
+                          clra_ram <= '1';     -- Increment ram key
+                          state_next <= s3;
+                       else
+                          inca_ram <= '1';     -- Increment ram key
+                          load_reg_a <= '1';   -- Load register
+                          load_reg_b <= '1';   -- Load register
+                          state_next <= s2;
+                       end if;
+                    end if;
+              end if;
+        end if;
+ 
+        -----------------
+        -- S2
+        -- Identical as s1 the only difference is mux control has been set in s2
+        -----------------
+        when s2 =>
+           if (rx_done='1') then
+              -- Check for valid ascii only for Alphabet lower and upper case, Space and Enter
+              if ((ascii_r >= x"41" and ascii_r <= x"5A") or
+                 (ascii_r >= x"61" and ascii_r <= x"7A") or
+                    ascii_r = x"20" or ascii_r = x"0D") then
+                    wr <= '1';
+ 
+                    if (ascii_r = x"20") then -- X"20" = "Space"-key
+                       inca_ram <= '1';
+                       state_next <= s2;
+                    else
+                       if (ascii_r=x"0D") then -- X"0D" = "Enter"-key (carriage return)
+                          clra_ram <= '1';
+                          state_next <= s3;
+                       else
+                          inca_ram <= '1';     -- Increment ram key
+                          mux_ctr_a <= '1';    -- Mux control while encryption,
+                          mux_ctr_b <= '1';    -- Mux control while decryption
+                          load_reg_a <= '1';   -- Load register
+                          load_reg_b <= '1';   -- Load register
+                       end if;
+                    end if;
+              end if;
+           end if;
+        -----------------
+        -- S3
+        -----------------
+        when s3 =>
+           if (ascii_t=x"0D") then
+           state_next <= s0;
+        else
+           tx_start <= '1';
+           state_next <= s4;
+        end if;
+        -----------------
+        -- S4
+        -----------------
+        when s4 =>
+           if (tx_done='1') then 
+              inca_ram <= '1';
+              state_next <= s3;
+           end if;
+     end case;
+  end process;
+ 
+  dec_mode <= dec;  -- Assigning encode/decode mode
+ 
 end arch;
+
